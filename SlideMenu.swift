324a4d356b22944392fde001513d7442353c3ab5
Uber - Abdul <abdul@zoojoo.be>
2017-02-21 15:25:40 +0530

    Initial Commit

---
+++ SlideMenu.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SliderMenu
+//
+//  Created by Uber - Abdul on 21/02/17.
+//  Copyright © 2017 example.com. All rights reserved.
+//
+
+import UIKit
+
+class SlideMenu: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.isNavigationBarHidden = true
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
